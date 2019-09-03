4c623fe980b67302a96ed6fb0c076d8c8d509a00
Aaron Sickler <sicklera517@gmail.com>
2019-09-03 21:42:06 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -4,8 +4,11 @@ var playlist = {
 
 function updatePlaylist(playlist, artistName, songTitle) {
   playlist[artistName] = songTitle
+
+  return playlist
 }
 
 function removeFromPlaylist(playlist, artistName, songTitle) {
   delete playlist[artistName]
+    return playlist
 }
